aee775e375e4afaade011730bfc12f4f6382ffda
Abhishek Galfade <106589511+thisisabhi12@users.noreply.github.com>
2024-02-26 06:14:28 +0000

    conversation of data types

---
+++ conversion_operation.js
@@ -0,0 +1,24 @@
+let score = "hitesh"
+
+console.log(typeof score);
+console.log(typeof(score));  // as a method 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33 => 33"
+//"33abc => NaN"
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true;  0>= false
+// "" => false
+//"abhi" => true
+// more than 1 is true
+// more than 0 iss true
