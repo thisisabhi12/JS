3aa86c620d500ea5ab0de7bb63621033c141f80a
Abhishek Galfade <106589511+thisisabhi12@users.noreply.github.com>
2024-02-27 10:39:05 +0000

    date and time function

---
+++ DataType.js
@@ -0,0 +1,39 @@
+// primitive
+
+
+// 7 types : String ,Number, Boolean, null, Undefined, Symbol, BigInt
+
+
+
+
+//Refrence(Non primitive)
+
+
+// array, objects, functions, 
+
+let myname = "abhi"
+let anothername = myname
+
+anothername = "Coffee" 
+
+console.log(myname);
+
+
+let userOne = {
+
+    email: "abhi@gmail.com",
+    upi: "abhi@oksbi"
+
+}
+
+let userTwo = userOne
+
+userTwo.email="yash@gmail.com"
+
+console.log(userTwo.email)
+console.log(userOne.email)
+
+
+
+
+
--- conversion_operation.js
+++ conversion_operation.js
@@ -1,21 +1,21 @@
-let score = "hitesh"
+// let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));  // as a method 
+// console.log(typeof score);
+// console.log(typeof(score));  // as a method 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33 => 33"
 //"33abc => NaN"
 // true => 1; false => 0
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true;  0>= false
 // "" => false
