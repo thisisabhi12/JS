80771a19d44868ad0adb3a494ee69831a6d7142c
Abhishek Galfade <106589511+thisisabhi12@users.noreply.github.com>
2024-02-28 08:10:53 +0000

    slice and splice

---
+++ array.js
@@ -0,0 +1,27 @@
+// array 
+// ( ) paranthesis 
+// [ ] square bracket
+// { } curly braces 
+ const myArr = [0, 1, 22, 3, 4, 5]
+// console.log(myArr);
+
+
+// const myArr2 = new Array("A","B","C","D")
+// // console.log(myArr2[0]);
+
+// myArr.unshift(99)
+// myArr.push(55)
+// myArr.pop()
+// console.log(myArr)
+
+
+const myn1 = [2, 0, 33, 44, 55 ]
+
+console.log("A " , "= " + myn1)
+
+const myn2 = myn1.slice(1,3)
+console.log(myn2)
+{
+const myn2 = myn1.splice(1,3)
+console.log(myn2)
+}
