f29c98d71545f904613314ad3ac4f0496d4fd916
Abhishek Galfade <106589511+thisisabhi12@users.noreply.github.com>
2024-03-07 10:43:45 +0000

    Truthy and falsy values and know ther basic opreatator

--- truthy.js
+++ truthy.js
@@ -18,3 +18,26 @@ if(userEmail) {
 if (userEmail.length === 0){
     console.log("Array is empty");
 }
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+val1 = null ?? 10 ?? 20
+ console.log(val1);
+
+
+ // Treniry Operator
+
+ //condition ? true : false
+
+ const iceTeaprice = 100;
+
+ iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80");;
+
+
+
