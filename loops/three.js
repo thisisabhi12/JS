3a5c94a1e5f1e96c5b95174f4c0e44b283bdba81
Abhishek Galfade <106589511+thisisabhi12@users.noreply.github.com>
2024-03-11 10:36:51 +0000

    for of and for in loop

---
+++ loops/three.js
@@ -0,0 +1,53 @@
+// for of
+
+
+
+// const arr = [1,2,3,4,5]
+
+
+// for (const num of arr ) {
+
+//     console.log(num);
+    
+// }
+
+// const  greetings = "Hello ji namaste"
+
+// for (const greet of greetings) {
+    
+//     console.log(`Greetings is ${greet}`);
+   
+// }
+
+
+// Maps 
+
+// const map =  new Map()
+// map.set('In', "India")
+// map.set('USA', "United states of America")
+// map.set('Fr', "France")
+// map.set('In', "India")
+
+// console.log(map)
+
+// for (const [key, value] of map) {
+
+//     console.log(key,':-',value)
+// }
+
+const myObject = {
+     js : 'javascript',
+     cpp : 'c++',
+     rb : "ruby",
+     swift : "swift by apple"
+}
+// for in loop
+for (const lang in myObject) {
+    console.log(`${lang} shortcut is for ${myObject[lang]}`);
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+ for (const key in object) {
+    
+ }
